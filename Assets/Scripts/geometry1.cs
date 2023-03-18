ee2e9704fe000860e61fd949024bcc7d41207d34
Michał Rogowski <85677540+Panzerski@users.noreply.github.com>
2023-03-18 17:55:23 +0100

    z ostrosłupa do czworościanu

--- Assets/Scripts/geometry1.cs
+++ Assets/Scripts/geometry1.cs
@@ -9,19 +9,14 @@ public class geometry1 : MonoBehaviour
 
     void Start()
     {
+        
         int i = 0;
         while(i<=p)
         {
             geo = Draw(geo, Mathf.Pow(2,i));
             i++;
         }
-
         GetComponent<MeshCombine>().CombineMeshes();
-
-        foreach (Transform child in transform)
-        {
-            Destroy(child.gameObject);
-        }
     }
 
     GameObject Draw(GameObject obj,float d)
@@ -30,15 +25,25 @@ public class geometry1 : MonoBehaviour
         parent.name = "rodzic rzedu " + d ;
         parent.AddComponent<MeshCombine>();
         parent.transform.SetParent(transform);
+        
         Instantiate(obj, parent.transform);
-        Instantiate(obj, new Vector3(parent.transform.position.x + d, parent.transform.position.y, parent.transform.position.z), Quaternion.identity, parent.transform);
-        Instantiate(obj, new Vector3(parent.transform.position.x + d, parent.transform.position.y, parent.transform.position.z + d), Quaternion.identity, parent.transform);
-        Instantiate(obj, new Vector3(parent.transform.position.x, parent.transform.position.y, parent.transform.position.z + d), Quaternion.identity, parent.transform);
-        Instantiate(obj, new Vector3(parent.transform.position.x + d/2, parent.transform.position.y + d, parent.transform.position.z + d/2), Quaternion.identity, parent.transform);
+        Instantiate(obj, new Vector3(parent.transform.position.x+1.4142f*d, parent.transform.position.y, parent.transform.position.z-0.8165f * d), parent.transform.rotation, parent.transform);
+        Instantiate(obj, new Vector3(parent.transform.position.x+ 0.94281f*d, parent.transform.position.y+1.3333f * d, parent.transform.position.z), parent.transform.rotation, parent.transform);
+        Instantiate(obj, new Vector3(parent.transform.position.x+1.4142f * d, parent.transform.position.y, parent.transform.position.z+0.8165f * d), parent.transform.rotation, parent.transform);
+
         parent.GetComponent<MeshCombine>().CombineMeshes();
-        //CombineMeshes(parent);
+
+        DestroyChildren(transform);
 
         return parent;
+    }
+
+    void DestroyChildren(Transform transform)
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
     }
 
     
